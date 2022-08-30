7a085d7c9ee79243a2ae24f9df116dafbb00666d
Abdelhadi Hussein <abod95@gmail.com>
2022-08-30 02:23:06 -0500

    Added frame module and other common angular material modules to app component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FrameModule } from './frame/frame.module';
+import { MatIconModule } from "@angular/material/icon";
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+
 
 @NgModule({
   declarations: [
-    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FrameModule,
+    MatIconModule,
+    MatNativeDateModule,
+    MatSnackBarModule,
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
