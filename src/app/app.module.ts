1390f127b72dc97d1640a7b923108d732b873ac7
Abdelhadi Hussein <abod95@gmail.com>
2022-09-17 00:11:23 -0500

    Added ngxs to the project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { FrameModule } from './frame/frame.module';
 import { MatIconModule } from "@angular/material/icon";
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { NgxsRouterPluginModule } from '@ngxs/router-plugin';
+import { NgxsModule } from '@ngxs/store';
 
 
 @NgModule({
@@ -21,7 +23,9 @@ import { MatSnackBarModule } from "@angular/material/snack-bar";
     FrameModule,
     MatIconModule,
     MatNativeDateModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    NgxsModule.forRoot([]),
+    NgxsRouterPluginModule.forRoot()
   ],
   bootstrap: [AppComponent]
 })
