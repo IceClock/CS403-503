17cd05e3ca8048e0e40faaa72a687acadd9e6736
Abdelhadi Hussein <abod95@gmail.com>
2022-09-26 09:59:50 -0500

    Added some PWA configs.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,10 +29,7 @@ import { environment } from '../environments/environment';
     NgxsModule.forRoot([]),
     NgxsRouterPluginModule.forRoot(),
     ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      enabled: environment.production
     })
   ],
   bootstrap: [AppComponent]
