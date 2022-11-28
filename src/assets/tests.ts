f4e6cb5edcde7601a979c4fc76db795f350d4e45
Abdelhadi Hussein <abod95@gmail.com>
2022-11-28 01:01:35 -0600

    Added closure and comments test cases.

--- src/assets/tests.ts
+++ src/assets/tests.ts
@@ -311,19 +311,326 @@ export const TESTS = {
   ],
   "closure" : [
     {
-      testLabel: '',
+      testLabel: 'Assign to closure',
+      testValue: 
+      `
+      var f;
+      var g;
+      
+      {
+        var local = "local";
+        fun f_() {
+          print local;
+          local = "after f";
+          print local;
+        }
+        f = f_;
+      
+        fun g_() {
+          print local;
+          local = "after g";
+          print local;
+        }
+        g = g_;
+      }
+      
+      f();
+      // expect: local
+      // expect: after f
+      
+      g();
+      // expect: after f
+      // expect: after g
+      `
+    },
+    {
+      testLabel: 'Assign to shadowed later',
+      testValue: 
+      `
+      var a = "global";
+
+      {
+        fun assign() {
+          a = "assigned";
+        }
+      
+        var a = "inner";
+        assign();
+        print a; // expect: inner
+      }
+      
+      print a; // expect: assigned
+      `
+    },
+    {
+      testLabel: 'Close over fucntion parameter',
+      testValue: 
+      `
+      var f;
+
+      fun foo(param) {
+        fun f_() {
+          print param;
+        }
+        f = f_;
+      }
+      foo("param");
+      
+      f(); // expect: param
+      `
+    },
+    {
+      testLabel: 'Close over later variable',
       testValue: 
       `
 
+      // This is a regression test. There was a bug where if an upvalue for an
+      // earlier local (here "a") was captured *after* a later one ("b"), then it
+      // would crash because it walked to the end of the upvalue list (correct), but
+      // then didn't handle not finding the variable.
+      
+      fun f() {
+        var a = "a";
+        var b = "b";
+        fun g() {
+          print b; // expect: b
+          print a; // expect: a
+        }
+        g();
+      }
+      f();
       `
     },
+    {
+      testLabel: 'Close over method parameter',
+      testValue: 
+      `
+      var f;
+
+      class Foo {
+        method(param) {
+          fun f_() {
+            print param;
+          }
+          f = f_;
+        }
+      }
+      
+      Foo().method("param");
+      f(); // expect: param
+      `
+    },
+    {
+      testLabel: 'Closed closure in function',
+      testValue: 
+      `
+      var f;
+
+      {
+        var local = "local";
+        fun f_() {
+          print local;
+        }
+        f = f_;
+      }
+      
+      f(); // expect: local
+      `
+    },
+    {
+      testLabel: 'Nested closure',
+      testValue: 
+      `
+      var f;
+
+      fun f1() {
+        var a = "a";
+        fun f2() {
+          var b = "b";
+          fun f3() {
+            var c = "c";
+            fun f4() {
+              print a;
+              print b;
+              print c;
+            }
+            f = f4;
+          }
+          f3();
+        }
+        f2();
+      }
+      f1();
+      
+      f();
+      // expect: a
+      // expect: b
+      // expect: c
+      `
+    },
+    {
+      testLabel: 'Open closure in function',
+      testValue: 
+      `
+      {
+        var local = "local";
+        fun f() {
+          print local; // expect: local
+        }
+        f();
+      }
+      `
+    },
+    {
+      testLabel: 'Open closure multiple times',
+      testValue: 
+      `
+      var f;
+
+      {
+        var a = "a";
+        fun f_() {
+          print a;
+          print a;
+        }
+        f = f_;
+      }
+      
+      f();
+      // expect: a
+      // expect: a
+      `
+    },
+    {
+      testLabel: 'Reuse closure slot',
+      testValue: 
+      `
+      {
+        var f;
+      
+        {
+          var a = "a";
+          fun f_() { print a; }
+          f = f_;
+        }
+      
+        {
+          // Since a is out of scope, the local slot will be reused by b. Make sure
+          // that f still closes over a.
+          var b = "b";
+          f(); // expect: a
+        }
+      }
+      `
+    },
+    {
+      testLabel: 'Shadow closure with local',
+      testValue: 
+      `
+      {
+        var foo = "closure";
+        fun f() {
+          {
+            print foo; // expect: closure
+            var foo = "shadow";
+            print foo; // expect: shadow
+          }
+          print foo; // expect: closure
+        }
+        f();
+      }
+      `
+    },
+    {
+      testLabel: 'Unused closure',
+      testValue: 
+      `
+      // This is a regression test. There was a bug where the VM would try to close
+      // an upvalue even if the upvalue was never created because the codepath for
+      // the closure was not executed.
+      
+      {
+        var a = "a";
+        if (false) {
+          fun foo() { a; }
+        }
+      }
+      
+      // If we get here, we didn't segfault when a went out of scope.
+      print "ok"; // expect: ok
+      `
+    },
+    {
+      testLabel: 'Unused later closure',
+      testValue: 
+      `
+      // This is a regression test. When closing upvalues for discarded locals, it
+      // wouldn't make sure it discarded the upvalue for the correct stack slot.
+      //
+      // Here we create two locals that can be closed over, but only the first one
+      // actually is. When "b" goes out of scope, we need to make sure we don't
+      // prematurely close "a".
+      var closure;
+      
+      {
+        var a = "a";
+      
+        {
+          var b = "b";
+          fun returnA() {
+            return a;
+          }
+      
+          closure = returnA;
+      
+          if (false) {
+            fun returnB() {
+              return b;
+            }
+          }
+        }
+      
+        print closure(); // expect: a
+      }
+      `
+    }
   ],
   "comments" : [
     {
-      testLabel: '',
+      testLabel: 'Line at EOF',
       testValue: 
       `
-
+      print "ok"; // expect: ok
+      // comment
+      `
+    },
+    {
+      testLabel: 'Only line comment',
+      testValue: 
+      `
+      // comment
+      `
+    },
+    {
+      testLabel: 'Only line comment and line',
+      testValue: 
+      `
+      // comment
+      `
+    },
+    {
+      testLabel: 'Unicode',
+      testValue: 
+      `
+      // Unicode characters are allowed in comments.
+      //
+      // Latin 1 Supplement: £§¶ÜÞ
+      // Latin Extended-A: ĐĦŋœ
+      // Latin Extended-B: ƂƢƩǁ
+      // Other stuff: ឃᢆ᯽₪ℜ↩⊗┺░
+      // Emoji: ☃☺♣
+      
+      print "ok"; // expect: ok
       `
     },
   ],
